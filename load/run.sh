d7294a053c77177b53cc662442885b1d68099244
Jelte Fennema <github-tech@jeltef.nl>
2015-06-02 10:26:48 +0200

    Change some stuff to be able to do actual tests

--- load/run.sh
+++ load/run.sh
@@ -6,16 +6,16 @@ log_dir=/var/log/nginx/
 echo "cd $log_dir;" rm cache.log | cat - purge_cache.sh \
     | ssh root@cache 'bash -s'
 
-tests='nocache.basic'
+tests='nocache.basic ismproxy basic single double'
 
 for prefix in $tests; do
     export URL_PREFIX=$prefix
     make clean
     make link
     make fmp4-iss-log
-    # make fmp4-dash-log
-    # make fmp4-hls-log
-    # make fmp4-hds-log
+    make fmp4-dash-log
+    make fmp4-hls-log
+    make fmp4-hds-log
     echo "cd $log_dir;" mv cache.log "$URL_PREFIX".log | cat - purge_cache.sh \
         | ssh root@cache 'bash -s'
 done
