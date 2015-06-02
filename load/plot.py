3b4c410c1a71e8c169cc021c80072a3640bfa87f
Jelte Fennema <github-tech@jeltef.nl>
2015-06-02 10:34:43 +0200

    Change the plotting code to use the new input

--- load/plot.py
+++ load/plot.py
@@ -1,17 +1,15 @@
 from pprint import pprint  # noqa
-import numpy as np
 import matplotlib.pyplot as plt  # noqa
 import seaborn as sns  # noqa
 import os
 import json
-from collections import defaultdict
 import pandas as pd
 
 sns.set_context("poster")
 
-BASE_PATH = os.path.abspath('data_tmp2')
+BASE_PATH = os.path.abspath('data_new')
 dirs = os.listdir(BASE_PATH)
-data = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
+data = []
 for d in dirs:
     dir_path = os.path.join(BASE_PATH, d)
     for filename in os.listdir(dir_path):
@@ -21,75 +19,56 @@ for d in dirs:
         video_type = '-'.join(filename.split('-')[1:3])
         file_path = os.path.join(dir_path, filename)
         with open(file_path) as f:
-            cur_d = data[d][video_type]
             after = None
-            test_type = None
+            run_type = None
             seconds = None
+            cur_d = None
             for l in f:
                 if l.startswith('run_type'):
-                    test_type = l.split()[-1]
+                    run_type = l.split()[-1]
                     after = None
-                elif l.startswith('elapsed time')
-                initial_seconds = float(lines[2].split()[-1])
-                summaries = {
-                    'initial': json.loads(lines[3].split()[-1]),
-                    'next': json.loads(lines[6].split()[-1]),
-                }
+                elif l.startswith('after'):
+                    after = l.split()[-1].split('-')[1]
 
-                cur_d['initial']['seconds'] = initial_seconds
-                cur_d['next']['seconds'] = summaries['next']['duration'] / 10**6
+                elif l.startswith('elapsed time'):
+                    seconds = float(l.split()[-1])
 
-                for k, summary in summaries.items():
-                    sec = cur_d[k]['seconds']
-                    cur_d[k]['requests'] = summary['requests']
-                    cur_d[k]['requests_per_sec'] = summary['requests'] / sec
-                    bytes_ = summary['bytes']
-                    cur_d[k]['bytes'] = bytes_
-                    cur_d[k]['mbps'] = summary['bytes'] / 10**6 / sec
+                elif l.startswith('summary'):
+                    cur_d = json.loads(l.split()[-1])
+                    cur_d['server_type'] = d
+                    cur_d['video_type'] = video_type[5:]
+                    cur_d['run_type'] = run_type
+                    cur_d['after'] = after
+
+                    if seconds is None:
+                        seconds = cur_d['duration'] / 10**6
+                    cur_d['seconds'] = seconds
+
+                    print(type(cur_d['requests']))
+                    print(seconds)
+                    cur_d['requests_per_second'] = cur_d['requests'] / seconds
+                    cur_d['MB/s'] = cur_d['bytes'] / 10**6 / seconds
+
+                    data.append(cur_d)
+                    seconds = None
+
+                elif len(l.split()) == 2:
+                    k = l.split()[0][:-1]  # remove the collon
+                    v = int(l.split()[-1])
+
+                    cur_d[k] = v
 
 
-l = []
-for server_type, server_data in data.items():
-    new_server_data = {}
-    for video_type, video_data in server_data.items():
-        for test_type, test_data in video_data.items():
-            for k, v in test_data.items():
-                l.append([server_type, video_type[5:], test_type, k, v])
-
-
-df = pd.DataFrame.from_records(l, columns=['server', 'video', 'test',
-                                           'property', 'value'])
+df = pd.DataFrame(data)
+df.fillna(0, inplace=True)
 print(df)
-for name, g in df.groupby('property'):
-    if name in ['seconds', 'requests']:
-        name = name.replace('_', ' ')
-        g = g.rename(columns={'value': name})
-        sns.factorplot('video', name, 'server', row='test', kind='bar',
-                       data=g, sharey=False)
-        plt.show()
 
-#
-# pprint(new_data)
-#
-# order = sorted(new_data['basic'].keys())
-# pprint(order)
-#
-# request_ps_data = {}
-#
-# for server_type, server_data in new_data.items():
-#     l = []
-#     request_ps_data[server_type] = l
-#     for test_type in order:
-#         l.append(server_data[test_type]['requests_per_sec'])
-#
-# pprint(request_ps_data)
-#
-# bar_width = 0.25
-# fig = plt.figure()
-# ax = fig.gca()
-# bargroup_starts = np.arange(len(order)) * 2
-# for i, (server_type, server_data) in enumerate(request_ps_data.items()):
-#     print(i, l)
-#     rects = ax.bar(bargroup_starts + i*bar_width, server_data)
-#
-# plt.show()
+for run_type, g in df.groupby('run_type'):
+    print(run_type)
+    print(g.max())
+    continue
+    for prop in ['MB/s', 'internal_bytes', 'internal_requests']:
+        sns.factorplot('video_type', prop, 'server_type', kind='bar',
+                       data=g, sharey=False)
+        plt.title(run_type)
+        plt.show()
