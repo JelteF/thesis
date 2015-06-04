913fcdf33a160a31689eedc239697a7b8ee63641
Jelte Fennema <github-tech@jeltef.nl>
2015-06-04 10:39:49 +0200

    Don't clean anymore, just append the results

--- load/run.sh
+++ load/run.sh
@@ -10,7 +10,7 @@ tests='nocache.cdn cdn ismproxy nocache.transmux single.transmux double.transmux
 
 for prefix in $tests; do
     export URL_PREFIX=$prefix
-    make clean
+    # make clean
     for con in 1 2 5 10 25 50; do
         export CONNECTIONS=$con
         make fmp4-dash-log
